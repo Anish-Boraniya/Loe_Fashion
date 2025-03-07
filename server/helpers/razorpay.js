f455f7103e5bc27f9fd60e8a22367f9a90cc2291
Anish-boraniya <anishboraniya2004@gmail.com>
2025-03-07 12:05:15 +0530

    changes

--- server/helpers/razorpay.js
+++ server/helpers/razorpay.js
@@ -1,7 +1,8 @@
 const razorpay = require("razorpay");
-
+console.log("Razorpay Key ID:", process.env.RAZORPAY_KEY_ID);
+console.log("Razorpay Key Secret:", process.env.RAZORPAY_KEY_SECRET);
 const razorpayInstance = new razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: process.env.RAZORPAY_KEY_ID ,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
