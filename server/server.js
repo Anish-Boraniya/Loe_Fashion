a2d0ff180e0bf6bb0885e60caf43c91b85ff12eb
Anish-boraniya <anishboraniya2004@gmail.com>
2025-04-07 19:19:27 +0530

    update server file

--- server/server.js
+++ server/server.js
@@ -28,12 +28,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://loe-fashion-1.onrender.com"
+];
+
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://loe-fashion-1.onrender.com"
-    ],    
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
